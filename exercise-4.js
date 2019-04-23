ac480499613c8344c686e255973531f19f48e0c6
mromiario <mromiario@gmail.com>
2019-04-23 14:59:50 +0700

    Exercise switch-case

---
+++ exercise-4.js
@@ -0,0 +1,63 @@
+var hari = 28; //Input tanggal
+var bulan =11; //Input bulan
+var namaBulan; //variabel nama bulan
+var tahun = 1997; //Input tahun
+
+switch(bulan){
+    case 1 :{
+        namaBulan = 'Januari';
+        break;
+    }
+    case 2 :{
+        namaBulan = 'Februari';
+        break;
+    }
+    case 3 :{
+        namaBulan = 'Maret';
+        break;
+    }
+    case 4 :{
+        namaBulan = 'April';
+        break;
+    }
+    case 5 :{
+        namaBulan = 'Mei';
+        break;
+    }
+    case 6 :{
+        namaBulan = 'Juni';
+        break;
+    }
+    case 7 :{
+        namaBulan = 'Juli';
+        break;
+    }
+    case 8 :{
+        namaBulan = 'Agustus';
+        break;
+    }
+    case 9 :{
+        namaBulan = 'September';
+        break;
+    }
+    case 10:{
+        namaBulan = 'Oktober';
+        break;
+    }
+    case 11:{
+        namaBulan = 'November';
+        break;
+    }
+    case 12:{
+        namaBulan = 'Desember';
+        break;
+    }
+    default :{
+        namaBulan = 'Bulan tidak valid' //Bulan selain 1-12
+        hari = '' // Reset hari
+        tahun = ''// Reset tahun
+    }
+
+}
+
+console.log(hari+' '+namaBulan+' '+tahun);
